00e629b10d957b1da7f9bdb2954aee081e59cfdc
Nayeon Kim <nykim92@gmail.com>
2022-03-10 01:13:37 -0800

    Tried a bunch of things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['Nunito','ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular']
+    },
     extend: {},
   },
   plugins: [],
